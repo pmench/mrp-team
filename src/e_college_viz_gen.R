81abe6eae5ba21835fd6576df9dfffa33ff5d2ba
Philip Menchaca <113058424+pmench@users.noreply.github.com>
2024-03-29 22:34:52 -0400

    Create working GIF

--- src/e_college_viz_gen.R
+++ src/e_college_viz_gen.R
@@ -1,18 +1,27 @@
-# Adapted from https://r-graph-gallery.com/a-smooth-transition-between-chloropleth-and-cartogram.html
+# Inspired by https://r-graph-gallery.com/a-smooth-transition-between-chloropleth-and-cartogram.html
 # using https://walker-data.com/census-r/mapping-census-data-with-r.html
 library(dplyr)
 library(tidyr)
 library(cartogram)
 library(ggplot2)
-library(tweenr)
+library(scales)
 library(gganimate)
 library(tigris)
 library(tidycensus)
 library(viridis)
+library(sf)
+
+NON_STATE_FIPS <- 57
+
+interpolate <- function (start, end, n) {
+  seq(from = start, to = end, length.out = n + 2)
+}
 
 # Get data
 e_college <- read.csv("../data/2020_ecollege_rep.csv")
-e_college %>% drop_na()
+e_college <- e_college %>%
+  mutate(STATEFP = as.numeric(STATEFP)) %>%
+  filter(STATEFP < NON_STATE_FIPS)
 
 usa <- get_acs(
   geography = "state",
@@ -22,71 +31,135 @@ usa <- get_acs(
   geometry = TRUE,
   resolution = "20m"
 ) %>%
+  filter(GEOID < NON_STATE_FIPS) %>%
+  mutate(GEOID = as.numeric(GEOID)) %>%
   shift_geometry()
 
-usa_pop_carto <- cartogram_cont(usa, "estimate")
+# Add geometries to ecollege data and normalize e-votes
+ecollege_geo <- merge(usa, e_college, by.x = "GEOID", by.y = "STATEFP", all.x = TRUE)
+ecollege_geo <- select(ecollege_geo, -c(STATE, STATENS, NAME, STATE_NAME, variable, moe))
+ecollege_geo$evotes_10M <- (ecollege_geo$e_votes / ecollege_geo$estimate) * 10000000
+col_position <- ncol(ecollege_geo) - 1
+ecollege_geo <- ecollege_geo[, c(1:(col_position-1), ncol(ecollege_geo), col_position)]
 
-# Create sf data with electoral colleges votes for each state
-usa_pop_carto_df <- as.data.frame(usa_pop_carto)
-usa_pop_carto_df$NAME <- tolower(usa_pop_carto_df$NAME)
-e_college_carto_df <- merge(usa_pop_carto_df, e_college, by.x = "NAME", by.y = "state", all.x = TRUE)
-e_college_carto_df <- select(e_college_carto_df, -c(STATE, STATEFP, STATENS, STATE_NAME))
-e_college_carto_df$e_votes_per_10M <- (e_college_carto_df$e_votes / e_college_carto_df$estimate) * 10000000
-e_college_sf <- left_join(usa_pop_carto, e_college_carto_df, by="GEOID", suffix = c("", ".y")) %>%
-  select(-ends_with(".y"))
-e_college_sf <- e_college_sf[!is.na(e_college_sf$e_votes), ]
-e_college_carto <- cartogram_cont(e_college_sf, weight = "e_votes_per_10M")
+# Interpolate values
+extract_e_votes <- ecollege_geo$evotes_10M
+extract_usa_pop <- ecollege_geo$estimate
+interpolated_values <- mapply(interpolate, start = extract_e_votes, end = extract_usa_pop, n = 2)
 
-# Create base visualizations
+# Build data for cartograms
+base_df <- select(ecollege_geo, -c(estimate, e_votes, evotes_10M))
+
+interpolated_values_df <- as.data.frame(interpolated_values)
+anim_frames <- list()
+for (i in 1:nrow(interpolated_values_df)) {
+  row_data <- as.data.frame(t(interpolated_values_df[i, ]))
+  anim_frames <- c(anim_frames, row_data)
+}
+
+### Adapted from ChatGPT ###
+anim_frames <- lapply(seq_along(anim_frames), function(i) {
+  transition_state <- as.data.frame(anim_frames[[i]])
+  colnames(transition_state) <- "value"
+  transition_state$id <- i
+  return(transition_state)
+})
+############################
+
+# Create cartograms
+for (i in seq_along(anim_frames)) {
+  anim_frames[[i]] <- bind_cols(base_df, anim_frames[[i]])
+  anim_frames[[i]] <- st_as_sf(anim_frames[[i]], sf_column_name = "geometry")
+  anim_frames[[i]] <- anim_frames[[i]] %>%
+    cartogram_cont(weight = "value")
+}
+
+# Build animation
+combined_anim_frames <- bind_rows(anim_frames)
+
+base_plot <- ggplot(combined_anim_frames) +
+  geom_sf(aes(fill = value)) +
+  scale_fill_viridis(
+    option = "mako",
+    labels = label_number_auto()
+  ) +
+  labs(title = "How Much Power Does Your Vote Have?", subtitle = "Distortion Between Elecltoral College Votes and Population") +
+  theme_void() +
+  theme(
+    text = element_text(color = "#22211d"),
+    plot.title = element_text(hjust = 0.5, family = "Montserrat", size = 8),
+    plot.subtitle = element_text(hjust = 0.5, family = "Montserrat", size = 5),
+    legend.text = element_text(family = "Montserrat", size = 5),
+    legend.title = element_text(family = "Montserrat", size = 5),
+    legend.key.size = unit(0.2, "cm")
+  )
+
+animated_plot <- base_plot +
+  transition_states(id) +
+  labs(title = 'How Much Power Does Your Vote Have?',
+       subtitle = "Distortion Between Electoral College Votes (Per 10M) & Population") +
+  enter_fade() +
+  exit_fade()
+
+animate(animated_plot, duration = 20, fps = 10, width = 1000, height = 800, res = 250)
+
+anim_save("../output/test_anim.gif", animation = last_animation(), dpi = 250)
+
+# Create static visualizations
+pop_carto <- cartogram_cont(last(anim_frames), weight = "value")
+
 jpeg(
-  "../output/test_cartpop.jpg",
+  "../output/pop_carto.jpg",
   width = 1000,
   height = 600,
   units = "px",
   res = 200
 )
 
-ggplot(data = usa_pop_carto) +
-  geom_sf(aes(fill = estimate)) +
+ggplot(data = pop_carto) +
+  geom_sf(aes(fill = value)) +
   scale_fill_viridis(
     option = "mako",
-    name = "POP2022 (M)" # Might need to fiddle with this as the scale changes between images
+    name = "Population (2022)"
   ) +
   labs(title = "How Much Power Does Your Vote Have?", subtitle = "Distortion by Population") +
   theme_void() +
   theme(
+    text = element_text(color = "#22211d"),
     plot.title = element_text(hjust = 0.5, family = "Montserrat", size = 10),
     plot.subtitle = element_text(hjust = 0.5, family = "Montserrat", size = 8),
     legend.text = element_text(family = "Montserrat", size = 5),
     legend.title = element_text(family = "Montserrat", size = 5)
   )
 
+dev.off()
+
+ecollege_carto <- cartogram_cont(first(anim_frames), weight = "value")
+
 jpeg(
-  "../output/test_cart_ecollege.jpg",
+  "../output/ecollege_carto.jpg",
   width = 1000,
   height = 600,
   units = "px",
   res = 200
 )
 
-ggplot(data = e_college_carto) +
-  geom_sf(aes(fill = e_votes_per_10M)) +
+ggplot(data = ecollege_carto) +
+  geom_sf(aes(fill = value)) +
   scale_fill_viridis(
     option = "mako",
-    name = "VOTES PER 10M" # Might need to fiddle with this as the scale changes between images
+    name = "Votes per 10M"
   ) +
   labs(title = "How Much Power Does Your Vote Have?", subtitle = "Distortion by Electoral College Votes") +
   theme_void() +
   theme(
+    text = element_text(color = "#22211d"),
     plot.title = element_text(hjust = 0.5, family = "Montserrat", size = 10),
     plot.subtitle = element_text(hjust = 0.5, family = "Montserrat", size = 8),
     legend.text = element_text(family = "Montserrat", size = 5),
     legend.title = element_text(family = "Montserrat", size = 5)
   )
-
-
 dev.off()
-
 
 
 
