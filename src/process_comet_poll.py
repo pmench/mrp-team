20ffc0c53e2ac5fa8ab1db7519a5d2f2959a2984
Philip Menchaca <113058424+pmench@users.noreply.github.com>
2024-04-04 17:12:50 -0400

    Complete working Comet polling data processing script

--- src/process_comet_poll.py
+++ src/process_comet_poll.py
@@ -1,7 +1,15 @@
-import pandas as pd
-from pandas.io.stata import StataReader
-import zipfile
 import os
+import zipfile
+
+import numpy as np
+import pandas as pd
+
+import helper as utl
+
+"""
+This script processes polling data from COMETrends surveys taken during the 2020 election.
+https://cometrends.utdallas.edu/data-and-questionnaires/
+"""
 
 
 def extract_zipped_data(path, destination, file_ext=".csv"):
@@ -46,6 +54,15 @@ def read_comet_poll(path):
 
 
 def select_comet_data(comet_data, cols_to_keep):
+    """
+    Selects columns in COMET data to keep and discards the rest.
+    :param comet_data: Comet
+    :type comet_data:
+    :param cols_to_keep:
+    :type cols_to_keep:
+    :return:
+    :rtype:
+    """
     comet_data = comet_data[cols_to_keep]
     return comet_data
 
@@ -63,17 +80,146 @@ def process_comet_data(comet_data, keep_all=False):
         "q56": "plan_to_vote_for",
         "regnz": "region",
     }
+
+    # Bucket and code age groups
     clean_comet_data = comet_data.rename(columns=col_rename)
+    vote_choice = [
+        "will vote for joe biden",
+        "voted for joe biden",
+        "voted for donald trump",
+        "will vote for donald trump",
+    ]
+    clean_comet_data = clean_comet_data[
+        (clean_comet_data["voted_for"].isin(vote_choice))
+        | (clean_comet_data["plan_to_vote_for"].isin(vote_choice))
+    ]
     clean_comet_data["birth_year"] = clean_comet_data["birth_year"].astype(int)
     age_bins = [0, 1965, 1985, 2002]
     age_encoding = [2, 1, 0]
     clean_comet_data["age_group"] = pd.cut(
         clean_comet_data["birth_year"], age_bins, right=True, labels=age_encoding
     )
+
+    # Recode gender
+    clean_comet_data["gender_coded"] = np.where(
+        clean_comet_data["gender"] == "male", 1, 0
+    )
+    clean_comet_data["gender_coded"] = clean_comet_data["gender_coded"].astype(
+        "category"
+    )
+
+    # Recode education
+    ed_conditions = [
+        clean_comet_data["education"] == "some high school or less",
+        clean_comet_data["education"] == "high school graduate",
+        clean_comet_data["education"] == "community college",
+        clean_comet_data["education"] == "some university",
+        clean_comet_data["education"] == "graduated university, b.a. or b.sc.",
+        clean_comet_data["education"] == "graduate or professional school",
+    ]
+    ed_codes = [1, 1, 2, 2, 2, 2]
+    clean_comet_data["education_coded"] = np.select(
+        ed_conditions, ed_codes, default=np.nan
+    )
+    clean_comet_data["education_coded"] = clean_comet_data["education_coded"].astype("category")
+
+    # Recode race
+    race_conditions = [
+        clean_comet_data["race"] == "white, non hispanic",
+        clean_comet_data["race"] == "african american",
+        clean_comet_data["race"] == "hispanic",
+        clean_comet_data["race"] == "asian",
+        clean_comet_data["race"] == "native american",
+        clean_comet_data["race"] == "other - please specify",
+    ]
+    race_codes = [1, 2, 4, 3, 9, 9]
+    clean_comet_data["race_coded"] = np.select(
+        race_conditions, race_codes, default=np.nan
+    )
+    clean_comet_data["race_coded"] = clean_comet_data["race_coded"].astype("category")
+
+    # Recode state
+    fips = utl.get_fips(
+        "https://www2.census.gov/geo/docs/reference/codes2020/national_state2020.txt"
+    )
+    fips["STATEFP"] = fips["STATEFP"].astype(int)
+    fips = fips[fips["STATEFP"] < 57]
+
+    clean_comet_data["state"] = clean_comet_data["state"].str.lower().str.strip()
+    fips["STATE_NAME"] = fips["STATE_NAME"].str.lower().str.strip()
+
+    merged_df = pd.merge(
+        clean_comet_data, fips, left_on="state", right_on="STATE_NAME", how="left"
+    )
+    clean_comet_data = merged_df.drop(columns=["STATE_NAME", "STATENS", "STATE"])
+    clean_comet_data["STATEFP"] = clean_comet_data["STATEFP"].astype("category")
+
+    # Recode most important issue
+    issue_condition = [
+        clean_comet_data["most_imp_issue"] == "education",
+        clean_comet_data["most_imp_issue"] == "environment",
+        clean_comet_data["most_imp_issue"] == "racism",
+        clean_comet_data["most_imp_issue"] == "police violence",
+        clean_comet_data["most_imp_issue"] == "health care",
+        clean_comet_data["most_imp_issue"] == "immigration",
+        clean_comet_data["most_imp_issue"] == "inequality in incomes & wealth",
+        clean_comet_data["most_imp_issue"] == "corona virus (covid-19) pandemic",
+        clean_comet_data["most_imp_issue"] == "poverty",
+        clean_comet_data["most_imp_issue"] == "economy",
+        clean_comet_data["most_imp_issue"] == "law & order",
+        clean_comet_data["most_imp_issue"] == "unemployment",
+        clean_comet_data["most_imp_issue"] == "other issue",
+        clean_comet_data["most_imp_issue"] == "don't know",
+    ]
+
+    issue_codes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    clean_comet_data["issue_coded"] = np.select(issue_condition, issue_codes, default=np.nan)
+    clean_comet_data["issue_coded"] = clean_comet_data["issue_coded"].astype("category")
+
+    # Recode vote choice
+    vote_condition = [
+        # Biden condition
+        (clean_comet_data["voted_for"].str.lower() == "voted for joe biden")
+        | (
+            clean_comet_data["plan_to_vote_for"].str.lower()
+            == "will vote for joe biden"
+        ),
+        # Trump condition
+        (clean_comet_data["voted_for"].str.lower() == "voted for donald trump")
+        | (
+            clean_comet_data["plan_to_vote_for"].str.lower()
+            == "will vote for donald trump"
+        ),
+    ]
+    vote_codes = [0, 1]
+    clean_comet_data["vote_coded"] = np.select(
+        vote_condition, vote_codes, default=np.nan
+    )
+    clean_comet_data["vote_coded"] = clean_comet_data["vote_coded"].astype("category")
+
+    # Recode region
+    region_condition = [
+        clean_comet_data["region"] == "Midwest",
+        clean_comet_data["region"] == "Northeast",
+        clean_comet_data["region"] == "South",
+        clean_comet_data["region"] == "West",
+    ]
+    region_codes = [2, 1, 3, 4]
+    clean_comet_data["region_coded"] = np.select(
+        region_condition, region_codes, default=np.nan
+    )
+    clean_comet_data["region_coded"] = clean_comet_data["region_coded"].astype("category")
+
+    # Return cleaned data
+    print(f"Dtypes Check:\n{clean_comet_data.dtypes}\n")
+    print(f"NaNs Check:\n{clean_comet_data.isna().sum().sum()}\n")
     if keep_all:
         return clean_comet_data
     else:
-        return clean_comet_data[col_rename.values()]
+        keep_cols = [
+            col for col in clean_comet_data.columns if col not in col_rename.values()
+        ]
+        return clean_comet_data[keep_cols]
 
 
 def main():
@@ -99,8 +245,10 @@ def main():
         "regnz",
     ]
     comet_data = select_comet_data(comet_data, comet_cols)
-    test = process_comet_data(comet_data, keep_all=True)
-    print("done")
+    clean_comet_data = process_comet_data(comet_data, keep_all=True)
+    comet_recoded = process_comet_data(comet_data, keep_all=False)
+    clean_comet_data.to_csv("../data/comet_polls/clean_comet.csv", index=False)
+    comet_recoded.to_csv("../data/comet_polls/comet_recoded.csv", index=False)
 
 
 if __name__ == "__main__":
